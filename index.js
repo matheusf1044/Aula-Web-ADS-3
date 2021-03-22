8140cefbede2d5173094bb02813953c4ace7e481
matheusf1044 <80286099+matheusf1044@users.noreply.github.com>
2021-03-22 19:01:04 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(
+    function () {
+        alert("O documento carregou!")
+    }
+)    
+
+$("#bt_inicio").click(
+    function(){
+        alert("ta errado meu pacero")
+    }
+)
